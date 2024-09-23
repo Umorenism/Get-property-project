6290606f7fbaae3b546195f8c671ee1d7e82eaa9
Umorenism <umorenismvic@gmail.com>
2024-09-23 03:23:41 -0700

    updated

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -1,96 +1,38 @@
-// import React, { useState } from "react";
-
-// const Login: React.FC = () => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   const handleSubmit = (e: React.FormEvent) => {
-//     e.preventDefault();
-//     // Handle login logic
-//     console.log({ email, password });
-//   };
-
-//   return (
-//     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-//       <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
-//         <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
-//         <form onSubmit={handleSubmit}>
-//           <div className="mb-4">
-//             <label className="block text-gray-700">Email</label>
-//             <input
-//               type="email"
-//               value={email}
-//               onChange={(e) => setEmail(e.target.value)}
-//               className="w-full px-3 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-//               placeholder="Enter your email"
-//               required
-//             />
-//           </div>
-//           <div className="mb-6">
-//             <label className="block text-gray-700">Password</label>
-//             <input
-//               type="password"
-//               value={password}
-//               onChange={(e) => setPassword(e.target.value)}
-//               className="w-full px-3 py-2 border rounded-lg text-gray-700 focus:outline-none focus:border-blue-500"
-//               placeholder="Enter your password"
-//               required
-//             />
-//           </div>
-//           <button
-//             type="submit"
-//             className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition-colors"
-//           >
-//             Login
-//           </button>
-//           <p className="text-center mt-4">
-//             Don't have an account?{" "}
-//             <a href="/signup" className="text-blue-500 hover:underline">
-//               Sign Up
-//             </a>
-//           </p>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Login;
-
 import React, { useState } from "react";
 
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai"; // Import icons for visibility toggle
-import axiosInstance from "../utlis/axiosInstance";
-import { useNavigate } from "react-router-dom";
+// import axiosInstance from "../utlis/axiosInstance";
+// import { useNavigate } from "react-router-dom";
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const [error, setError] = useState("");
-  const navigate = useNavigate()
+  const [error] = useState("");
+  // const navigate = useNavigate();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const response = await axiosInstance.post("/auth/login", {
-        email,
-        password,
-      });
-      const {token} =response.data
-      console.log(response.data); //
-    } catch (err) {
-      console.error(err);
-      setError("Login failed. Please check your credentials.");
-    }
-  };
+  // const handleSubmit = async (e: React.FormEvent) => {
+  //   e.preventDefault();
+  //   try {
+  //     const response = await axiosInstance.post("/auth/login", {
+  //       email,
+  //       password,
+  //     });
+  //     const { token } = response.data;
+  //     console.log(response.data); //
+  //   } catch (err) {
+  //     console.error(err);
+  //     setError("Login failed. Please check your credentials.");
+  //   }
+  // };
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
         <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
         <p>Welcome Back! Get ready to unlock the best Real Estate deal</p>
-        <form onSubmit={handleSubmit}>
+        <form>
+          {/* <form onSubmit={handleSubmit}> */}
           <div className="mb-4">
             <label className="block text-gray-700">Email</label>
             <input
